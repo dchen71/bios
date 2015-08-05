bb6fe0458f621b2687a50975192dad0ab223c77d
Dan <dchen12812@gmail.com>
2015-08-05 15:51:24 -0700

    added redirect for blank

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem 'bio'
-gem 'client_side_validations'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,20 @@
 class StaticPagesController < ApplicationController
 	def index
-
+		@sp = StaticPage.new
 	end
 
 	def search
-		@seq = Bio::Sequence::NA.new(params[:nucleotide])
+		@seq = ''
+		if search_params[:nucleotide].length > 0
+			@seq = Bio::Sequence::NA.new(search_params[:nucleotide])
+		else
+			flash[:error] = "No string entered"
+			redirect_to root_path
+		end
+	end
+
+	private
+	def search_params
+		params.require(:static_page).permit(:nucleotide)
 	end
 end

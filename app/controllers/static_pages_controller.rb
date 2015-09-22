57d79f4a3a7f0475f97258f49e11bddfddedca94
Dan <dchen12812@gmail.com>
2015-09-21 17:33:20 -0700

    added bar graph showing distriution of bases

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,11 @@ class StaticPagesController < ApplicationController
 			seq = Bio::Sequence::NA.new(search_params[:nucleotide])
 			if seq.illegal_bases.length == 0
 				@seq = Bio::Sequence::NA.new(search_params[:nucleotide])
+				@a = (@seq.composition['a'].to_f/@seq.length * 100).round(2) 
+				@t = (@seq.composition['t'].to_f/@seq.length * 100).round(2) 
+				@g = (@seq.composition['g'].to_f/@seq.length * 100).round(2) 
+				@c = (@seq.composition['c'].to_f/@seq.length * 100).round(2) 
+				@u = (@seq.composition['u'].to_f/@seq.length * 100).round(2) 
 			else
 				flash[:error] = "Bases can only contain ATGC"
 				redirect_to root_path
